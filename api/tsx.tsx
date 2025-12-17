01a04908ab7572401ab32499139e7e459ec3a485
piet-rian <piet_rian@protonmail.com>
2025-12-17 17:24:37 +0900

    コントローラー的なルーティングの仕方を検討する

--- api/tsx.tsx
+++ api/tsx.tsx
@@ -1,4 +1,4 @@
-import { Context } from "@hono";
+import { Context, Hono } from "@hono";
 
 const Heading = (props: { msg: string }) => {
   return <h1>Hello {props.msg}!</h1>;
@@ -8,16 +8,24 @@ const View = (props: { msg: string }) => {
   return (
     <html>
       <body>
+        {/* title等に対してrollupを行う機能がhonoに備わっているため、こういった具合の定義をして良い */}
+        <title>{props.msg}</title>
         <Heading msg={props.msg} />
       </body>
     </html>
   );
 };
 
-export function Get(c: Context): Response | Promise<Response> {
-  console.log("GET /tsx");
+// ある程度の関連性があるパス群をまとめる場合には、Honoインスタンスを分ける&exportする形が良い
+// (main.ts側でapp.route("/tsx", tsxroute);のように使用する)
+const localApp = new Hono();
 
+localApp.get("/", (c: Context) => {
   const name = c.req.query("name") || "Hono TSX";
 
+  console.log(`name: ${name}`);
+
   return c.html(<View msg={name} />);
-}
+});
+
+export default localApp;
--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import { logger } from "@hono/logger";
 import { etag } from "@hono/etag";
 import { prettyJSON } from "@hono/pretty-json";
 import * as about from "./api/about.ts";
-import * as tsx from "./api/tsx.tsx";
+import { default as tsxroute } from "./api/tsx.tsx";
 import * as rootest from "./api/rootest.ts";
 
 const app = new Hono();
@@ -13,7 +13,11 @@ app.use(etag()).use(logger()).use(prettyJSON());
 
 app.get("/", (c: Context): Response => c.text("Hono!"));
 app.get("/about", about.Get);
-app.get("/tsx", tsx.Get);
+
+// honoのベストプラクティスとしては「コントローラーを作るな(≒パス定義と実装を近くにせよ)」であるが
+// ある程度の単位でまとめるのであればこうするかな、という感じ
+app.route("/tsx", tsxroute);
+
 app.get("/rootest", rootest.Get); // クエリパラメーターは定義不要
 
 export default app;
