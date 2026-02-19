69a709cca34533c3731c6247ad3503b7eb6247b0
piet-rian <piet_rian@protonmail.com>
2026-02-19 20:54:43 +0900

    jsを文字通り生で見る機会は少ないと判断してミドルウェアを除去

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ import { Context, Hono } from "@hono";
 // * deno & jsrのhono の場合、ミドルウェアのインポートは `@hono/*` となる
 import { logger } from "@hono/logger";
 import { etag } from "@hono/etag";
-import { prettyJSON } from "@hono/pretty-json";
 import * as about from "@api/about.ts";
 import { default as tsxroute } from "@api/tsx.tsx";
 import * as rootest from "@api/rootest.ts";
@@ -12,7 +11,7 @@ import { uid } from "./mw/uid.ts";
 const app = new Hono();
 
 app.use(uid);
-app.use(etag()).use(logger()).use(prettyJSON());
+app.use(etag()).use(logger());
 
 app.get("/", (c: Context): Response => c.text("Hono!"));
 app.get("/about", about.Get);
