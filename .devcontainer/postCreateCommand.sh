b4ea29f9097d8a66838ab0658269c5abe27565f6
piet-rian <piet_rian@protonmail.com>
2026-02-22 11:07:15 +0900

    git config 指定をローカルで動かす方式わかったのでそっちに修正

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,26 +12,33 @@
 ### スクリプト内でエラーが発生した場合に即座に終了するためのオプション
 set -e
 
+### tmp内作業が終了した後で、戻って来るプロジェクトルートを記録
+PROJECT_ROOT=`pwd`
+
 ### 以下、tmp以下のディレクトリで実施する
 mkdir /tmp/dev-container-post-create-user-define-actions
 cd /tmp/dev-container-post-create-user-define-actions
 
 ##### 本体部分 #####
 #
-# 本プロジェクトにおいては、現時点では feature などで事が足りているため何もしない
+# 現時点では feature などで事が足りているため何もしない
 #
+####################
 
 ##### git ローカル設定 #####
+# .git ディレクトリのある場所に戻る必要があるので tmp から脱出し、ワーキングディレクトリに戻る
+cd ${PROJECT_ROOT}
+
 # see. https://git-scm.com/docs/git-config
 # devcontainer環境なら global 指定でも他に影響が出ないはずなので良いだろうという判断
-git config --global fetch.prune true
-git config --global fetch.pruneTags true
-git config --global log.date iso8601
-git config --global blame.date iso8601
-git config --global merge.ff false # 必ずマージコミットを作る
-git config --global merge.autoStash true
-git config --global pull.autoStash true
-git config --global core.longpaths true # パス長すぎ対策
+git config --local  fetch.prune true
+git config --local  fetch.pruneTags true
+git config --local  log.date iso8601
+git config --local  blame.date iso8601
+git config --local  merge.ff false # 必ずマージコミットを作る
+git config --local  merge.autoStash true
+git config --local  pull.autoStash true
+git config --local  core.longpaths true # パス長すぎ対策
 
 ##### print completion message #####
 echo "postCreateCommand.sh: \033[42m completed. \033[0m"
