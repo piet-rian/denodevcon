171fe6c33e4c44481cccfda306ebcd36c1d485dc
piet-rian <piet_rian@protonmail.com>
2026-02-22 11:27:38 +0900

    テストの追加

--- src/tests/api/about.test.ts
+++ src/tests/api/about.test.ts
@@ -31,3 +31,65 @@ Deno.test("notExits", async () => {
   const res = await app.request("/notExits");
   assertEquals(res.status, 404);
 });
+
+Deno.test("fetch", async () => {
+  const res = await app.request("/about");
+  assertEquals(res.status, 200);
+  const responseJson = await res.json();
+
+  assert("name" in responseJson);
+  assert("site" in responseJson);
+
+  assertEquals(responseJson.name, "foobars");
+  assertEquals(responseJson.site, "norhe pole");
+});
+
+Deno.test("rootest(empty)", async () => {
+  const res = await app.request("/rootest");
+
+  assertEquals(res.status, 200);
+  const responseJson = await res.json();
+
+  assert("status" in responseJson);
+  assert("date" in responseJson);
+
+  assertEquals(responseJson.status, "success");
+  assertEquals(responseJson.date, "asc");
+});
+
+Deno.test("rootest(other)", async () => {
+  const res = await app.request("/rootest?date=foobar");
+
+  assertEquals(res.status, 200);
+  const responseJson = await res.json();
+
+  assert("status" in responseJson);
+  assert("date" in responseJson);
+
+  assertEquals(responseJson.status, "success");
+  assertEquals(responseJson.date, "asc");
+});
+Deno.test("rootest(asc)", async () => {
+  const res = await app.request("/rootest?date=asc");
+
+  assertEquals(res.status, 200);
+  const responseJson = await res.json();
+
+  assert("status" in responseJson);
+  assert("date" in responseJson);
+
+  assertEquals(responseJson.status, "success");
+  assertEquals(responseJson.date, "asc");
+});
+Deno.test("rootest(dsc)", async () => {
+  const res = await app.request("/rootest?date=desc");
+
+  assertEquals(res.status, 200);
+  const responseJson = await res.json();
+
+  assert("status" in responseJson);
+  assert("date" in responseJson);
+
+  assertEquals(responseJson.status, "success");
+  assertEquals(responseJson.date, "desc");
+});
