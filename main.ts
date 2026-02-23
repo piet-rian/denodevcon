583c530166238a061f9cbcee7b60cb64cc24cad6
piet-rian <piet_rian@protonmail.com>
2026-02-24 02:07:30 +0900

    インポートの並び替え

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { Context, Hono } from "@hono";
 // * deno & jsrのhono の場合、ミドルウェアのインポートは `@hono/*` となる
-import { logger } from "@hono/logger";
-import { etag } from "@hono/etag";
 import * as about from "@api/about.ts";
-import { default as tsxroute } from "@api/tsx.tsx";
-import * as rootest from "@api/rootest.ts";
 import * as fetchHandler from "@api/fetch.ts";
+import * as rootest from "@api/rootest.ts";
+import { default as tsxroute } from "@api/tsx.tsx";
+import { etag } from "@hono/etag";
+import { logger } from "@hono/logger";
 import { uid } from "@mw/uid.ts";
 
 const app = new Hono();
