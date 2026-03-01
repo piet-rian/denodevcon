76e879f236416246a54125ea11f2e43334e3c38f
piet-rian <piet_rian@protonmail.com>
2026-03-01 10:46:02 +0900

    grafana lgtm周りの諸々の明示化
    - configファイルを指定するように(配置場所はかなり暫定)
    - lokiでspan.attribute単位で絞り込む方法が結局わからなかったことを受けてのコメントおよびドキュメントの若干の変更

--- src/mw/uid.ts
+++ src/mw/uid.ts
@@ -1,16 +1,14 @@
 import { Context, Next } from "@hono";
 import { trace } from "@opentelemetry/api";
 
-// ヘッダーからUIDを取得し、OpenTelemetryのスパンに設定するミドルウェア
+/**
+ * HTTPヘッダからUIDを取得し、OpenTelemetryのスパンに設定するミドルウェア
+ */
 export const uid = (c: Context, next: Next) => {
   const uidFromHeader = c.req.header("X-User-ID") || "anonymous";
 
   const activeSpan = trace.getActiveSpan();
   activeSpan?.setAttribute("user.id", uidFromHeader);
 
-  // TODO: grafana lokiでuidを元にログをフィルタリングできるようにする
-  // * Tempoでuid単位で「トレース」をフィルタリングすることはできている
-  // * loki側でuidを元に「ログライン」をフィルタリングできるようにするにはどうすれば良いのかがわかっていない
-  // * lokiだとlabelという概念でしかフィルタリングが出来ないので、送出なり集計(OTELコレクターのプロセッサ？)の段階でuidをlabelとするような設定が必要？
   return next();
 };
